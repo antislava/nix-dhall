0974eefa293c8453c6551ae259d4be58192feb8f
Anti Slava <antislava@gmail.com>
2019-03-20 16:01:38 +0100

    Laoding bash completions in shellHook

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Nix overlays with `Dhall` release binaries
+# Nix overlays with `Dhall` release binaries
+
+## Prerequisites for updating GitHub release data used by nix derivations
+
+```shell
+make shell-dev
+```
+
+## Test with
+
+```shell
+nix-shell --pure ./test-shell.nix
+```
--- test-shell.nix
+++ test-shell.nix
@@ -1,13 +1,27 @@
 with import <nixpkgs> { overlays = [ (import ./.) ] ;};
-runCommand "dhall-haskell-all" {
+runCommand "dhall-haskell-all" rec {
   buildInputs = [
     dhall-bin
     dhall-json-bin
     dhall-text-bin
     dhall-bash-bin
   ];
+  # TODO: Possible to automate sourcing bash completions somehow?
+  # https://github.com/NixOS/nixpkgs/issues/44434#issuecomment-410624170
+  # ls $HOME/.nix-profile/etc/bash_completion.d
   shellHook = ''
     echo "Dhall core tools shell"
-    '';
+    echo "Loading bash completions:"
+    '' + (
+      builtins.concatStringsSep "\n" (
+        map (p:
+        ''
+          # echo ${p}
+          for e in ${p}/etc/bash_completion.d/*
+          do
+            echo `basename $e`
+            source $e
+          done
+        '') buildInputs
+      ));
 } ""
-
